ac42fa1a71d884f4b84f05a4f5718342c8d39352
Paulina Kuranowska <kuranowskapaulina@gmail.com>
2020-04-09 11:13:42 +0200

    end modul 7.4 initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,7 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
       console.log('new Product:', thisProduct);
     }
@@ -76,6 +77,43 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
       console.log(thisProduct.element);
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: click event listener to trigger */
+      clickableTrigger.addEventListener('click', function (event) {
+        console.log(clickableTrigger);
+
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* toggle active class on element of thisProduct */
+        thisProduct.element.classList.toggle('active');
+
+        /* find all active products */
+        const allActiveProducts = document.querySelectorAll(select.all.menuProductsActive);
+
+        /* START LOOP: for each active product */
+        for (let activeProduct of allActiveProducts) {
+
+          /* START: if the active product isn't the element of thisProduct */
+          if (activeProduct !== thisProduct.element) {
+
+            /* remove class active for the active product */
+            activeProduct.classList.remove('active');
+            console.log(activeProduct);
+
+            /* END: if the active product isn't the element of thisProduct */
+          }
+          /* END LOOP: for each active product */
+        }
+        /* END: click event listener to trigger */
+      });
     }
   }
 

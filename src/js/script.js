8181c88ce25732faf338b12548607e1332f8f434
Paulina Kuranowska <kuranowskapaulina@gmail.com>
2020-04-19 20:28:38 +0200

    modul 7.6 - finish processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -90,6 +90,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion() {
@@ -167,6 +168,7 @@
         /* save the element in thisProduct.data.params with key paramId as const param */
         const param = thisProduct.data.params[paramId];
         console.log(param);
+        console.log(paramId);
 
         /* START LOOP: for each optionId in param.options */
         for (let optionId in param.options) {
@@ -188,11 +190,42 @@
 
           /* START ELSE IF: if option is not selected and option is default */
           else if (!optionSelected && option.default) {
-            
+
             /* deduct price of option from price */
             price -= option.price;
 
-          /* END ELSE IF: if option is not selected and option is default */
+            /* END ELSE IF: if option is not selected and option is default */
+          }
+          /* find all images in thisProduct.imageWrapper */
+          const allImages = thisProduct.imageWrapper.querySelectorAll('.' + paramId + '-' + optionId);
+          console.log(allImages);          
+
+          /* START IF: if option is selected */
+          if (optionSelected) {
+
+            /* START LOOP: for every image in all images */
+            for (let image of allImages) {
+
+              /* add class active saved in classNames.menuProduct.imageVisible to image */
+              image.classList.add(classNames.menuProduct.imageVisible);
+              console.log(image);
+
+            /* END LOOP: for every image in all images */
+            }
+          /* END IF: if option is selected */
+          }
+          /* START ELSE */
+          else {
+
+            /* START LOOP: for every image in all images */
+            for (let image of allImages) {
+
+              /* remove class from image */
+              image.classList.remove(classNames.menuProduct.imageVisible);
+
+            /* END LOOP: for every image in all images */
+            }
+          /* END ELSE */
           }
 
           /* END LOOP: for each optionId in param.options */

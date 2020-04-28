bbdc0e867944b86631024b0d84cbc46f52afe3f4
Paulina Kuranowska <kuranowskapaulina@gmail.com>
2020-04-28 12:31:09 +0200

    finish modul 8.4

--- src/js/script.js
+++ src/js/script.js
@@ -363,7 +363,9 @@
     announce() {
       const thisWidget = this;
 
-      const event = new Event('updated');
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
       thisWidget.element.dispatchEvent(event);
     }
   }
@@ -405,7 +407,10 @@
       thisCart.dom.toggleTrigger.addEventListener('click', function (event) {
         event.preventDefault();
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
 
+      thisCart.dom.productList.addEventListener('updated', function(){
+        thisCart.update();
       });
     }
 
